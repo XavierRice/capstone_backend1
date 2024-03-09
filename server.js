343f1d8a6fd8cfbd05a1491b7439a32a62a289a2
Xavier Rice <Xavier.Rice@gmail.com>
2024-03-09 17:58:46 -0500

    db not working. stripe connected to server

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-const app = require('./app')
-require('dotenv').config()
+const app = require("./app");
+require("dotenv").config();
+const PORT = process.env.PORT;
+const stripeSecret = process.env.X_STRIPE_SECRET;
 
-const PORT = process.env.PORT
+
+const Stripe = require("stripe")
+const stripe = Stripe(stripeSecret)
+
+async function createStripeAccount(){
+    try {
+        const account = await stripe.accounts.create({
+            country: 'US',
+            type: 'custom',
+            capabilities: {
+              card_payments: {
+                requested: true,
+              },
+              transfers: {
+                requested: true,
+              },
+            },
+          });
+          console.log(`Stripe Account Created: ${account?.id}` )
+    } catch (error){
+        console.error('Error creating Stripe Account', error.message)
+    }
+}
+createStripeAccount()
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
-})
+  console.log(`Server is running on port: ${PORT}`);
+});
