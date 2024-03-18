b1ec69b06590d2f81e859052d7dccdacfe0e55f4
Xavier Rice <Xavier.Rice@gmail.com>
2024-03-18 16:11:56 -0400

    working on socket

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
 const cors = require('cors')
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173", 
+      methods: ["GET", "POST"],
+    }
+  });
+
+//Controllers
 const usersController = require('./controllers/usersController')
 const eventsController = require('./controllers/eventsController')
 const donationsController = require('./controllers/donationsController')
 const newsController = require('./controllers/newsController')
 const stripeController = require('./controllers/stripeController')
-const {stripeCSPMiddleware} = require('./controllers/stripeMiddleware')
+const { stripeCSPMiddleware } = require('./controllers/stripeMiddleware')
 
 // Middleware
 app.use(cors())
@@ -14,15 +25,22 @@ app.use(express.json())
 app.use('/users', usersController)
 app.use('/events', eventsController)
 app.use('/donations', donationsController)
-
 app.use('/register', usersController)
-
 app.use('/news', newsController)
-
 app.use('/payments', stripeCSPMiddleware, stripeController)
 
+// socket.IO SERVER
+io.on('connection', (socket) => {
+    console.log('user connection baby!');
+    socket.emit('testEvent', { msg: 'Hello from server!' });
+    socket.on('disconnect', ()=>{
+        console.log('we have disconnected you & me')
+    });
+});
 
-
+setInterval(() => {
+    io.emit('heartbeat', { timestamp: Date.now() });
+}, 10000)
 
 app.get('/', (req, res) => {
     res.send( 'Welcome to Impactify')
